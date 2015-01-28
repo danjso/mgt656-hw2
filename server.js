1edffafda72015b489b2729fec54c95d87701094
Dan Jso <daniel.oliveira@yale.edu>
2015-01-28 07:30:55 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/netid', function(request, response){
+    response.send('excited crocodile');
+});
+
+
 app.listen(process.env.PORT || 4000);
